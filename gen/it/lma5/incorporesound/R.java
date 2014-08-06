42da174f9b3fad18f7e68321d07d68554977eadf
Luca <luca.luca.fanelli@gmail.com>
2014-08-06 16:31:36 +0200

    grafica main activity completata

--- gen/it/lma5/incorporesound/R.java
+++ gen/it/lma5/incorporesound/R.java
@@ -27,10 +27,15 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int kaydet=0x7f020001;
-        public static final int launcher=0x7f020002;
-        public static final int myluncher=0x7f020003;
+        public static final int addplaylist=0x7f020000;
+        public static final int changeplaylist=0x7f020001;
+        public static final int deleteplaylist=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int kaydet=0x7f020004;
+        public static final int launcher=0x7f020005;
+        public static final int myluncher=0x7f020006;
+        public static final int playplaylist=0x7f020007;
+        public static final int wallpaper=0x7f020008;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f090029;
