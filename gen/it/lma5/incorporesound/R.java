82c931c78b0558242868bb1cd10bd03f5041bb16
Luca <luca.luca.fanelli@gmail.com>
2014-08-18 10:18:16 +0200

    aggiunto indicatore canzone in corso

--- gen/it/lma5/incorporesound/R.java
+++ gen/it/lma5/incorporesound/R.java
@@ -35,16 +35,17 @@ public final class R {
         public static final int forward=0x7f020005;
         public static final int ic_launcher=0x7f020006;
         public static final int kaydet=0x7f020007;
-        public static final int launcher=0x7f020008;
-        public static final int minus=0x7f020009;
-        public static final int myluncher=0x7f02000a;
-        public static final int pause=0x7f02000b;
-        public static final int play=0x7f02000c;
-        public static final int playplaylist=0x7f02000d;
-        public static final int plus=0x7f02000e;
-        public static final int save=0x7f02000f;
-        public static final int stop=0x7f020010;
-        public static final int wallpaper=0x7f020011;
+        public static final int key=0x7f020008;
+        public static final int launcher=0x7f020009;
+        public static final int minus=0x7f02000a;
+        public static final int myluncher=0x7f02000b;
+        public static final int pause=0x7f02000c;
+        public static final int play=0x7f02000d;
+        public static final int playplaylist=0x7f02000e;
+        public static final int plus=0x7f02000f;
+        public static final int save=0x7f020010;
+        public static final int stop=0x7f020011;
+        public static final int wallpaper=0x7f020012;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f090029;
